de61b91925083b0f7779ddac273512584d6e0937
trooperV <victor.cholakov@abv.bg>
2017-08-09 19:41:46 +0300

    regex

---
+++ C#/C# Fundamentals/9. Regular Expressions/Ex1_Extract_Emails/Ex1_Extract_Emails/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ex1_Extract_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(?<=\s)([a-zA-Z0-9]+)(_?|\.?|-?)([a-zA-Z0-9]+)*@[a-zA-Z]+\-?[a-zA-Z]+\.[a-zA-Z]+(\.[a-zA-Z]+)?";
+
+            MatchCollection emails = Regex.Matches(Console.ReadLine(), pattern);
+
+            foreach (Match email in emails)
+            {
+                Console.WriteLine(email.Value);
+            }
+        }
+    }
+}
