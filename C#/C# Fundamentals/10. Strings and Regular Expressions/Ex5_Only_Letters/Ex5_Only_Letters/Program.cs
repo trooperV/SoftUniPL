97f7d961f932b11c272531a49361c79e610f5432
trooperV <victor.cholakov@abv.bg>
2017-08-12 00:31:48 +0300

    reg Ex / strinG

---
+++ C#/C# Fundamentals/10. Strings and Regular Expressions/Ex5_Only_Letters/Ex5_Only_Letters/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ex5_Only_Letters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            MatchCollection matches = Regex.Matches(input, @"(?<digit>\d+)(?<char>[A-Za-z])", RegexOptions.RightToLeft);
+
+            StringBuilder sb = new StringBuilder(input);
+
+
+            foreach (Match match in matches)
+            {
+                sb.Replace(match.Groups["digit"].ToString(), match.Groups["char"].ToString());
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
---
+++ C#/C# Fundamentals/10. Strings and Regular Expressions/Ex6_Email_Statistics/Ex6_Email_Statistics/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ex6_Email_Statistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"^(?<user>[A-Za-z]{5,})@(?<domain>[a-z]{3,}\.(com|bg|org))$";
+            Regex regex = new Regex(pattern);
+
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, List<string>> userDomain = new Dictionary<string, List<string>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (regex.IsMatch(input))
+                {
+                    Match uderD = Regex.Match(input, pattern);
+                    string username = uderD.Groups["user"].ToString();
+                    string domain = uderD.Groups["domain"].ToString();
+
+                    if (!userDomain.ContainsKey(domain))
+                    {
+                        userDomain[domain] = new List<string>();
+                    }
+
+                    if (!userDomain[domain].Contains(username))
+                    {
+                        userDomain[domain].Add(username);
+                    }
+                }
+            }
+
+            foreach (KeyValuePair<string, List<string>> domain in userDomain.OrderByDescending(a => a.Value.Count))
+            {
+                Console.WriteLine(domain.Key + ":");
+                foreach (string user in domain.Value)
+                {
+                    Console.WriteLine("### " + user);
+                }
+            }
+        }
+    }
+}
