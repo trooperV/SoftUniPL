f60e4ff5012690154489f7ae6c641d01aa26272b
trooperV <victor.cholakov@abv.bg>
2017-08-12 12:50:00 +0300

    string regex end

---
+++ C#/C# Fundamentals/10. Strings and Regular Expressions/Ex7_Hideout/Ex7_Hideout/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Ex7_Hideout
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                string[] com = Console.ReadLine().Split();
+                char c = char.Parse(com[0]);
+                int n = int.Parse(com[1]);
+
+                Regex regex = new Regex($@"\{c}{{{n},}}");
+
+                if (regex.IsMatch(input))
+                {
+                    MatchCollection matches = Regex.Matches(input, $@"\{c}{{{n},}}");
+
+                    int index = matches[0].Index;
+                    int length = matches[0].Length;
+
+                    Console.WriteLine($"Hideout found at index {index} and it is with size {length}!");
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ C#/C# Fundamentals/10. Strings and Regular Expressions/Ex8_Mines/Ex8_Mines/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex8_Mines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetIn(new StreamReader(Console.OpenStandardInput(500)));
+
+            string input = Console.ReadLine();
+            StringBuilder sb = new StringBuilder(input);
+            int startIndex = 0;
+            int endIndex = 0;
+
+            while ((startIndex = input.IndexOf('<', endIndex)) != -1 &&
+                    (endIndex = input.IndexOf('>', startIndex)) != -1)
+            {
+                string bombChars = input.Substring(startIndex + 1, 3);
+                int bombStrength = Math.Abs(bombChars[0] - bombChars[1]);
+
+                int leftIndex = Math.Max(0, startIndex - bombStrength);
+                int length = Math.Min(endIndex + bombStrength, input.Length - 1);
+
+                for (int i = leftIndex; i <= length; i++)
+                {
+                    sb[i] = '_';
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
