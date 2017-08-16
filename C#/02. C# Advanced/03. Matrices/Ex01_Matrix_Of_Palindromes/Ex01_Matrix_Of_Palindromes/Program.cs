eb7acd8d1657b4a57cb6079086d32d61f7688839
trooperV <victor.cholakov@abv.bg>
2017-08-16 21:48:51 +0300

    arrs

---
+++ C#/02. C# Advanced/03. Matrices/Ex01_Matrix_Of_Palindromes/Ex01_Matrix_Of_Palindromes/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_Matrix_Of_Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rc = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int r = rc[0];
+            int c = rc[1];
+
+            string[,] matrix = new string[r,c];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i,j] = "" + (char)('a'+ i) + (char)('a'+(j+i)) + (char)('a' + i);
+                }
+            }
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C#/02. C# Advanced/03. Matrices/Ex02_Diagonal_Difference/Ex02_Diagonal_Difference/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex02_Diagonal_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[][] arr = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = new int[n];
+                arr[i] = Console.ReadLine().Split(new char[] {' '}, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            }
+
+            int j = 0;
+            int sum = 0;
+            for (int i = n - 1; i >= 0; i--)
+            {
+                sum += arr[j][i];
+                j++;
+            }
+
+            j = 0;
+            int sum2 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                sum2 += arr[j][i];
+                j++;
+            }
+
+            Console.WriteLine(Math.Abs(sum - sum2));
+        }
+    }
+}
