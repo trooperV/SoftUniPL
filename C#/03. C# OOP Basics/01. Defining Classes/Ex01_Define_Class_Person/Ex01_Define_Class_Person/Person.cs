8eeba75be4fefc5cd737034a2f5423abb958a79b
trooperV <victor.cholakov@abv.bg>
2017-09-02 15:31:47 +0300

    c# oop basics

---
+++ C#/03. C# OOP Basics/01. Defining Classes/Ex01_Define_Class_Person/Ex01_Define_Class_Person/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01_Define_Class_Person
+{
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+    }
+}
---
+++ C#/03. C# OOP Basics/01. Defining Classes/Ex01_Define_Class_Person/Ex01_Define_Class_Person/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Ex01_Define_Class_Person
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type personType = typeof(Person);
+            FieldInfo[] fields = personType.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            Console.WriteLine(fields.Length);
+        }
+    }
+}
